f0564b04a171f287642917f19dcb2777a5d86cc9
ehutzle <ehutzle@gmail.com>
2023-07-10 17:22:21 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+`git clone https://github.com/NvChad/NvChad ~/.config/nvim/lua/custom/  --depth 1`
---
+++ configs/lspconfig.lua
@@ -0,0 +1,19 @@
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require "lspconfig"
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+})
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
